6eb9666f28ec7755765ca1eb7df1b1cd9897b6b9
Chandler Mayo <chandler@chandlermayo.com>
2020-03-26 14:47:33 -0400

    Added PubNub

---
+++ rss-firehose/main.go
@@ -0,0 +1,16 @@
+// Stream randomly sorted rss feeds to PubNub
+
+package main
+
+import (
+//pubnub "github.com/pubnub/go"
+)
+
+func main() {
+	/*config := pubnub.NewConfig()
+	config.PublishKey = "pub-c-aac19938-466b-4d89-8a61-ba29ec3b4149"
+	config.SubscribeKey = "sub-c-0b04217e-6f8c-11ea-bbe3-3ec3e5ef3302"
+	channel := "news_stream"
+	pn := pubnub.NewPubNub(config)
+	pn.Publish().Channel(channel).Message("").Execute()*/
+}

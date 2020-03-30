1d9f6ff8e7372abde2fee578953a97bbc18db98b
Chandler Mayo <chandler@chandlermayo.com>
2020-03-30 17:04:53 -0400

    Styling

--- rss-firehose/main.go
+++ rss-firehose/main.go
@@ -51,7 +51,6 @@ func main() {
 			time.Sleep(time.Duration(rand.Int31n(5)) * time.Second) // Send random news every few seconds
 			return
 		}
-		_, ok := feed.Items[rand.Intn(len(feed.Items))]
 		pn.Publish().Channel(channel).Message(feed.Items[rand.Intn(len(feed.Items))]).Execute()
 		time.Sleep(time.Duration(rand.Int31n(5)) * time.Second) // Send random news every few seconds
 	}
